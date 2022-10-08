8ffe29f3bd12468459c71ca3e1c21098d1a69985
RobertCardoso <robert.rcardoso@hotmail.com>
2022-10-07 20:35:25 -0500

    Updates

---
+++ precache-manifest.ff2a34729cadb13df457cd42c0fcaecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f0ff8b2622dba137b31",
+    "url": "/static/css/main.f9d74e0b.chunk.css"
+  },
+  {
+    "revision": "8f0ff8b2622dba137b31",
+    "url": "/static/js/main.8f0ff8b2.chunk.js"
+  },
+  {
+    "revision": "7576f02ebf960f6fa9d8",
+    "url": "/static/js/1.7576f02e.chunk.js"
+  },
+  {
+    "revision": "9f111bdb3fbcafd7d074",
+    "url": "/static/js/2.9f111bdb.chunk.js"
+  },
+  {
+    "revision": "d8cf3946ea1208106c2a",
+    "url": "/static/js/runtime~main.d8cf3946.js"
+  },
+  {
+    "revision": "f866b9d9e0cba6a9e545e9224e100369",
+    "url": "/static/media/aevent.f866b9d9.jpg"
+  },
+  {
+    "revision": "2afa3f453be1fca07ea3275b09304684",
+    "url": "/index.html"
+  }
+];
